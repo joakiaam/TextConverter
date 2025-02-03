c7cc677ecb7e90867599cbd50874a1253741d329
joakiaam <joak171@gmail.com>
2025-02-03 16:04:26 +0100

    Created class that replaces the first word with a target words

---
+++ src/main/java/ReplaceFirstTextCommand.java
@@ -0,0 +1,11 @@
+public class ReplaceFirstTextCommand extends ReplaceTextCommand {
+
+  public ReplaceFirstTextCommand(String target, String replacement) {
+    super(target, replacement);
+  }
+
+  @Override
+  public String execute(String text) {
+    return text.replaceFirst(getTarget(), getReplacement());
+  }
+}
