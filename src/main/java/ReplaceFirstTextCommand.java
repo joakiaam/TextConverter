4ae118d2df82671fd6513f05bcb86cf095470581
joakiaam <joak171@gmail.com>
2025-02-03 16:30:58 +0100

    Added Java-doc comments

--- src/main/java/ReplaceFirstTextCommand.java
+++ src/main/java/ReplaceFirstTextCommand.java
@@ -1,9 +1,27 @@
+/**
+ * The ReplaceFirstTextCommand class extends the ReplaceTextCommand class
+ * and provides functionality to replace the first occurrence of a target
+ * string with a replacement string in a given text.
+ */
 public class ReplaceFirstTextCommand extends ReplaceTextCommand {
 
+  /**
+   * Constructs a new ReplaceFirstTextCommand with the specified target and replacement strings.
+   *
+   * @param target the string to be replaced
+   * @param replacement the string to replace the target with
+   */
   public ReplaceFirstTextCommand(String target, String replacement) {
     super(target, replacement);
   }
 
+  /**
+   * Executes the replace first command on the given text.
+   * Replaces the first occurrence of the target string with the replacement string.
+   *
+   * @param text the text in which the replacement is to be made
+   * @return the text with the first occurrence of the target string replaced by the replacement string
+   */
   @Override
   public String execute(String text) {
     return text.replaceFirst(getTarget(), getReplacement());
--- src/main/java/ReplaceTextCommand.java
+++ src/main/java/ReplaceTextCommand.java
@@ -1,20 +1,48 @@
+/**
+ * The ReplaceTextCommand class implements the TextCommand interface
+ * and provides functionality to replace occurrences of a target string
+ * with a replacement string in a given text.
+ */
 public class ReplaceTextCommand implements TextCommand {
   protected String target;
   protected String replacement;
 
+  /**
+   * Constructs a ReplaceTextCommand with the specified target and replacement strings.
+   *
+   * @param target the string to be replaced
+   * @param replacement the string to replace the target with
+   */
   public ReplaceTextCommand(String target, String replacement) {
     this.target = target;
     this.replacement = replacement;
   }
 
+  /**
+   * Executes the replace command on the given text.
+   *
+   * @param text the text in which to replace occurrences of the target string
+   * @return the text with the target string replaced by the replacement string
+   */
+  @Override
   public String execute(String text) {
     return text.replace(getTarget(), getReplacement());
   }
 
+  /**
+   * Returns the target string to be replaced.
+   *
+   * @return the target string
+   */
   public String getTarget() {
     return target;
   }
 
+  /**
+   * Returns the replacement string.
+   *
+   * @return the replacement string
+   */
   public String getReplacement() {
     return replacement;
   }
