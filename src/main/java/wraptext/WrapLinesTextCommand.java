90f1fb57eed950fec6542b7ea7d6fc31dd238c3e
joakiaam <joak171@gmail.com>
2025-02-06 13:36:00 +0100

    Fixed WrapLinesTextCommand so it only returns the new wrapped text

--- src/main/java/wraptext/WrapLinesTextCommand.java
+++ src/main/java/wraptext/WrapLinesTextCommand.java
@@ -1,12 +1,20 @@
 package wraptext;
 
 public class WrapLinesTextCommand extends WrapTextCommand {
+  private String nyText = "";
 
   public WrapLinesTextCommand(String opening, String end) {
     super(opening, end);
   }
+
   @Override
   public String execute(String text) {
+    String[] parts = text.split("\n");
+
+    for (String part : parts) {
+      nyText += getOpening() + part + getEnd() + "\n";
+    }
+    text = nyText;
     return text;
   }
 }
